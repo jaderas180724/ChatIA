ce392e75dbc382e7f38512cf8ac62d0ba619d88c
jaderas180724 <joshuaminariascos6@gmail.com>
2026-05-15 18:12:32 -0500

    Add Telegram icon component

---
+++ src/components/Auth/TelegramIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TelegramIcon = ({ size = 20, className = '' }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221l-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.446 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.121l-6.869 4.326-2.96-.924c-.643-.204-.658-.643.136-.953l11.566-4.458c.538-.196 1.006.128.832.941z" />
+  </svg>
+)
+
+export default TelegramIcon
